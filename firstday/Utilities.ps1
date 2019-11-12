0b6be71d8defe2a023ee04e550f1d8e32406f5e5
Sebastian Lopez <selopez@microsoft.com>
2019-11-12 15:32:27 -0300

    Add utilities for learning more things in web

---
+++ firstday/Utilities.ps1
@@ -0,0 +1,4 @@
+# Create a custom object to use for the Select-Object example.
+$object = [pscustomobject]@{Name="CustomObject";Expand=@(1,2,3,4,5)}
+# Use the ExpandProperty parameter to Expand the property.
+$object | Select-Object -ExpandProperty Expand -Property Name
