0db7550f481c8639ff785dec945cd1d395df6da0
Sebastian Lopez <selopez@microsoft.com>
2020-05-19 18:05:46 -0500

    starting module 8

--- MonthOfLunches/part2.ps1
+++ MonthOfLunches/part2.ps1
@@ -1,1 +1,11 @@
-#6.6. Common points of confusion
+Get-PSSnapin -Registered
+# add-pssnapin sqlservercmdletsnapin100 # to add snapin
+# get commands
+Get-Command -pssnapin sqlservercmdletsnapin100
+Get-Help *network*
+Get-Help Get-SmbServerNetworkInterface
+## using modules
+Import-Module -Name DnsClient
+get-Command -Module DnsClient
+Get-Help Clear-DnsClientCache -Verbose # More details in component
+Chapter 8. Objects: data by another name
