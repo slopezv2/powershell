8dbda3a65dd2804243dd85bff2782ab382e80aec
Sebastian Lopez <selopez@microsoft.com>
2020-05-21 18:14:46 -0500

    In module 9.6

--- MonthOfLunches/part2.ps1
+++ MonthOfLunches/part2.ps1
@@ -8,4 +8,26 @@ Get-Help Get-SmbServerNetworkInterface
 Import-Module -Name DnsClient
 get-Command -Module DnsClient
 Get-Help Clear-DnsClientCache -Verbose # More details in component
-Chapter 8. Objects: data by another name
+####### Objects
+Get-Process | Get-Member
+# Sorting 
+Get-Process | Sort-Object -Property VM -desc
+# Filtering properties
+Get-Process | Select-Object -Property Name,ID,VM,PM
+Get-Process | Get-Member | Get-Member
+# Random
+Get-Random
+# Current date-time 
+Get-Date
+# Hotfixes
+Get-HotFix | Sort-Object InstalledOn | Select-Object InstalledOn,InstalledBy,HotFixID
+# getting the services for a pc in a file and addiing properties
+Get-Content .\computers.txt | Get-Service
+Get-Process -Name note* | Stop-Process
+Import-Csv .\newUser.csv |
+Select-Object -Property *, 
+@{name = "SamAccountName"; expression = {$_.login}},
+@{label = 'Name'; expression = {$_.login}},
+@{n='Department'; e={$_.Dept}} |
+New-AdUser
+#9.6. Parenthetical commands
