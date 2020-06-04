f3819f1885ced1cf4bab4e0a72e4dcbcbbc5eb5d
Sebastian Lopez <selopez@microsoft.com>
2020-06-03 19:51:43 -0500

    Starting: chapter 14

--- MonthOfLunches/part3.ps1
+++ MonthOfLunches/part3.ps1
@@ -1,2 +1,12 @@
 Get-Privilege
 ## Chapter 13. Remote control: one-to-one, and one-to-many
+# Enable remoting session
+Enable-PSRemoting
+# Cambio de puerto para conexión remota
+Winrm set winrm/config/listener?Address=*+Transport=HTTP
+@{Port="1234"}
+# Start session
+Enter-PSSession -computerName Server-R2
+# One to many commands
+Invoke-Command -computerName Server-R2,Server-DC4,Server12 -command { Get-EventLog Security -newest 200 | Where { $_.EventID -eq 1212 }}
+# Chapter 14. Using Windows Management Instrumentation and CIM
